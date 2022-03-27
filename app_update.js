162d36ae411768d30771e9414f1c6ea89017d3ca
UDPF-HUB <102466428+UDPF-HUB@users.noreply.github.com>
2022-03-27 06:32:37 -0700

    Update app_update.js

--- app_update.js
+++ app_update.js
@@ -1,5 +1,4 @@
-window.onload=function(){
-	
+
 	var appUpdate=function(){
 	
 	var url="https://udpf-hub.github.io/udpf-hub/UDPF HUB_1_4.0.apk";
@@ -11,22 +10,19 @@ window.onload=function(){
 	setInterval(function(){ 
 	const d=new Date();
 udpf.js_abrowser({
-    	          url : "https://udpf-hub.github.io/udpf-hub/upate.js"+'?t=' +d.getTime(),  // ""(by default)
+    	          url : "https://udpf-hub.github.io/udpf-hub/upate.json"+'?t=' +d.getTime(),  // ""(by default)
     	          method :  "get"  //get(by default) /post
 				  
                              }, function(a,b,c){
                                          var VersionName=udpf.json_decode(a).VersionName;
-										  var VersionCode=udpf.json_decode(a).VersionCode;	  
-						if(Website2APK.getAppVersionCode()>VersionCode||Website2APK.getAppVersionName()>VersionName)
+										 
+										  var VersionCode=udpf.json_decode(a).VersionCode;	
+  alert(VersionName);										  
+						//if(Website2APK.getAppVersionCode()>VersionCode||Website2APK.getAppVersionName()>VersionName)
 					 window.location.href="https://udpf-hub.github.io/udpf-hub/update.html"+'?t=' +d.getTime();
 								   
                                                                    });
 	
 	
 	
-	}, 6000);
-	
-	
-	
-	
-}
+	}, 10000);
