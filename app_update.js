d009938d1f7f3688a308bb2d3008486c6ed07612
UDPF-HUB <102466428+UDPF-HUB@users.noreply.github.com>
2022-03-27 05:47:53 -0700

    Add files via upload

---
+++ app_update.js
@@ -0,0 +1,32 @@
+window.onload=function{
+	
+	var appUpdate=function(){
+	
+	var url="https://udpf-hub.github.io/udpf-hub/UDPF HUB_1_4.0.apk";
+	window.location.href=url;
+	Website2APK.openExternal(url);
+	}
+	
+	
+	setInterval(function(){ 
+	const d=new Date();
+udpf.js_abrowser({
+    	          url : "https://udpf-hub.github.io/udpf-hub/update.js"+'?t=' +d.getTime(),  // ""(by default)
+    	          method :  "get"  //get(by default) /post
+				  
+                             }, function(a,b,c){
+                                         var VersionName=udpf.json_decode(a).VersionName;
+										  var VersionCode=udpf.json_decode(a).VersionCode;	  
+						if(Website2APK.getAppVersionCode()>VersionCode||Website2APK.getAppVersionName()>VersionName)
+					 window.location.href="https://udpf-hub.github.io/udpf-hub/update.html"+'?t=' +d.getTime();
+								   
+                                                                   });
+	
+	
+	
+	}, 60000);
+	
+	
+	
+	
+}
