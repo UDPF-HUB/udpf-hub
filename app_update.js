813dec0474b2d8ebfad9936ef4f7fd31e7ba43ac
UDPF-HUB <102466428+UDPF-HUB@users.noreply.github.com>
2022-04-04 03:30:05 -0700

    Update app_update.js

--- app_update.js
+++ app_update.js
@@ -54,7 +54,7 @@ ext.main.plyr_init_mod_jionews();
 
 
 function hotstar_test(){
-var url="https://api.hotstar.com/play/v4/playback/content/1260009172?device-id=7abb02fb-427c-49d8-85e2-f0dab835528e&desired-config=audio_channel:stereo|container:fmp4|dynamic_range:sdr|encryption:widevine|ladder:tv|package:dash|resolution:fhd|video_codec:h264";
+var url="https://api.hotstar.com/play/v4/playback/content/1260090245?device-id=7abb02fb-427c-49d8-85e2-f0dab835528e&desired-config=audio_channel:stereo|container:fmp4|dynamic_range:sdr|encryption:widevine|ladder:tv|language:hin|package:dash|resolution:fhd|video_codec:h264";
 var header={
     "accept": "*/*",
     "accept-language": "en-US,en;q=0.9,fil;q=0.8",
@@ -65,7 +65,7 @@ var header={
     "x-hs-usertoken": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOiJ1bV9hY2Nlc3MiLCJleHAiOjE2NDkxMzM1NzksImlhdCI6MTY0OTA0NzE3OSwiaXNzIjoiVFMiLCJqdGkiOiIwNTIwYWJjY2NmM2M0NWUwYWZlOGIxOTQ2YmI0MDE1NSIsInN1YiI6IntcImhJZFwiOlwiNzE0M2M2NTYxNGU0NGMxOGI5MGEwNTFiNzU0OGQ5YjhcIixcInBJZFwiOlwiMzJkZDFjMDIzYmE4NDkzYzhmNjRjMmUzNGI5MzhiMDlcIixcIm5hbWVcIjpcIkJodXZhblwiLFwicGhvbmVcIjpcIjcyNzgwNTE5NzZcIixcImlwXCI6XCIyNDAxOjQ5MDA6Mzg1Yjo1YTg0OjY1MDQ6YzBmODo4OTFkOjJlOFwiLFwiY291bnRyeUNvZGVcIjpcImluXCIsXCJjdXN0b21lclR5cGVcIjpcIm51XCIsXCJ0eXBlXCI6XCJwaG9uZVwiLFwiaXNFbWFpbFZlcmlmaWVkXCI6ZmFsc2UsXCJpc1Bob25lVmVyaWZpZWRcIjp0cnVlLFwiZGV2aWNlSWRcIjpcIjdhYmIwMmZiLTQyN2MtNDlkOC04NWUyLWYwZGFiODM1NTI4ZVwiLFwicHJvZmlsZVwiOlwiQURVTFRcIixcInZlcnNpb25cIjpcInYyXCIsXCJzdWJzY3JpcHRpb25zXCI6e1wiaW5cIjp7XCJIb3RzdGFyTW9iaWxlXCI6e1wic3RhdHVzXCI6XCJTXCIsXCJleHBpcnlcIjpcIjIwMjMtMDEtMTJUMjA6MTI6NDQuMDAwWlwiLFwic2hvd0Fkc1wiOlwiMVwiLFwiY250XCI6XCIxXCJ9fX0sXCJlbnRcIjpcIkNnMFNDd2dCT0FGQUFWRFFCVmdCQ2pjS0JRb0RDZ0VGRWk0U0IyRnVaSEp2YVdRU0EybHZjeElIYW1sdkxXeDVaaG9DYzJRYUFtaGtJZ056WkhJcUJuTjBaWEpsYjFnQkNpSUtHZ29PRWdVMU5UZ3pOaElGTmpRd05Ea0tDQ0lHWm1seVpYUjJFZ1E0WkZnQkNwb0JDZ1VLQXdvQkFCS1FBUklIWVc1a2NtOXBaQklEYVc5ekVnZHFhVzh0YkhsbUVnbGhibVJ5YjJsa2RIWVNCbVpwY21WMGRoSUhZWEJ3YkdWMGRoSUVjbTlyZFJJRGQyVmlFZ1J0ZDJWaUVnZDBhWHBsYm5SMkVnVjNaV0p2Y3hJR2FtbHZjM1JpRWdwamFISnZiV1ZqWVhOMEVnUjBkbTl6RWdSd1kzUjJFZ05xYVc4YUFuTmtHZ0pvWkNJRGMyUnlLZ1p6ZEdWeVpXOVlBUklKQ0FFUTRJejB2Tm93XCIsXCJpc3N1ZWRBdFwiOjE2NDkwNDcxNzk3NTN9IiwidmVyc2lvbiI6IjFfMCJ9.p5Ioqoa4BmTiHQ4QXRCjMPg2rqGypPU9Tgj50nkV7nI"
   };
   
-var data='{"os_name":"Android","os_version":"10","app_name":"mobile","app_version":"7.34.1","platform":"Chrome","platform_version":"100.0.4896.60","client_capabilities":{"ads":["non_ssai"],"audio_channel":["stereo"],"dvr":["short"],"package":["dash","hls"],"dynamic_range":["sdr"],"video_codec":["h264"],"encryption":["widevine"],"ladder":["tv"],"container":["fmp4"],"resolution":["hd"]},"drm_parameters":{"widevine_security_level":["SW_SECURE_DECODE","SW_SECURE_CRYPTO"],"hdcp_version":["HDCP_NO_DIGITAL_OUTPUT"]},"resolution":"auto"}';
+var data='{"os_name":"Windows","os_version":"10","app_name":"web","app_version":"7.34.1","platform":"Chrome","platform_version":"100.0.4896.60","client_capabilities":{"ads":["non_ssai"],"audio_channel":["stereo"],"dvr":["short"],"package":["dash","hls"],"dynamic_range":["sdr"],"video_codec":["h264"],"encryption":["widevine"],"ladder":["tv"],"container":["fmp4"],"resolution":["hd"]},"language":"hin","drm_parameters":{"widevine_security_level":["SW_SECURE_DECODE","SW_SECURE_CRYPTO"],"hdcp_version":["HDCP_NO_DIGITAL_OUTPUT"]},"resolution":"auto"}';
 alert(udpf.js_browser(url,"post", data,header));
  //alert("hi1");
 	
