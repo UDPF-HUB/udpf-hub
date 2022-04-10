6c1df958656e1bc6b40797c405a30b997e8f21bd
UDPF-HUB <102466428+UDPF-HUB@users.noreply.github.com>
2022-04-10 11:53:11 -0700

    Update app_home.js

--- app_home.js
+++ app_home.js
@@ -115,7 +115,7 @@ var op=udpf.js_browser(url,"post", data,header);
 var url=udpf.json_decode(op.trim()).data.playback_sets[0].playback_url ;
 	
 document.getElementById("ext_player_url").src =url;
- ext.main.plyr_init();
+ ext.main.plyr_init_mod_hotstar();
 	//alert("hi1");
 	
 }
