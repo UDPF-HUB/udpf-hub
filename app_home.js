34598c27fbfbe5a8b1667e5d90e45070bf007167
UDPF-HUB <102466428+UDPF-HUB@users.noreply.github.com>
2022-04-10 11:18:37 -0700

    Update app_home.js

--- app_home.js
+++ app_home.js
@@ -21,8 +21,15 @@ xhr.setRequestHeader('Accept-Encoding' , 'gzip, deflate, br');
 xhr.setRequestHeader('Accept-Language','en-US,en;q=0.9');
 xhr.setRequestHeader('Host' , uu.host);
 xhr.setRequestHeader('Connection' , ' keep-alive');
-xhr.setRequestHeader('Origin','https://www.hotstar.com/');
+xhr.setRequestHeader('Origin','https://www.hotstar.com');
 xhr.setRequestHeader('Referer','https://www.hotstar.com/');
+xhr.setRequestHeader('DNT',' 1');
+xhr.setRequestHeader('Sec-Fetch-Dest',' empty');
+xhr.setRequestHeader('Sec-Fetch-Mode',' cors');
+xhr.setRequestHeader('Sec-Fetch-Site',' same-site');
+xhr.setRequestHeader('sec-ch-ua','" Not A;Brand";v="99", "Chromium";v="100", "Google Chrome";v="100"');
+xhr.setRequestHeader('sec-ch-ua-mobile','?0');
+xhr.setRequestHeader('sec-ch-ua-platform','"Windows"  ');
 xhr.setRequestHeader('Cookie', 'hs_uid=7abb02fb-427c-49d8-85e2-f0dab835528e; _gcl_au=1.1.928997546.1648449143; _gid=GA1.2.1712776197.1649560167; _clck=rx3blk|1|f0i|0; ajs_group_id=null; ajs_user_id="32dd1c023ba8493c8f64c2e34b938b09"; ajs_anonymous_id="b596d8cd-1397-4fe1-b68b-cfe0352a2b1d"; hdntl=exp=1649670671~acl=*ipl2022*~id=2c12db22367aa0662005342ceccaf547~data=ip=aJ6vCdzIRPZnvvjes033FQ-userid=kzk5JqhGGF7g9aYcHnC6fmW8idiF5H4Iif60dcnTPe9b-did=82d9Dx9Bl3dCUvTPdGUlTXK8Q4ZgfwpaUZU0DYBfYTbqVTmkIa3Ck6q-cc=in-~hmac=3c6128d3b2832483c956530312c49c0b7f03a5d9b492a0b27f97c984f53d4d2d; _ga=GA1.2.790039680.1648449145; _gat_UA-53733575-1=1; _uetsid=a1844240b87b11ecb2ec5348c8f30318; _uetvid=d4b635c0ae6011ec809949a1843510cb; _clsk=1rip6j5|1649612249144|3|0|i.clarity.ms/collect; _ga_QV5FD29XJC=GS1.1.1649612241.17.1.1649612254.47'); 
 xhr.setRequestHeader('User-Agent' , ' Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.80 Safari/537.36 Edg/98.0.1108.43');
  }
