c43bb66d15efb7c47232990cb08e4d71cdb6c999
UDPF-HUB <102466428+UDPF-HUB@users.noreply.github.com>
2022-04-13 00:19:10 -0700

    Update app_home.js

--- app_home.js
+++ app_home.js
@@ -2,6 +2,7 @@
 $('#app-main').on('DOMSubtreeModified', function(entries) {
 	for(var i=0; i<$("img").length;i++){
 	      $("img")[i].setAttribute("loading","lazy");
+	}
 	$("img").error(function(){ console.log("Image Broken");this.src='https://reactnativecode.com/wp-content/uploads/2018/02/Default_Image_Thumbnail.png';});		
                         
 });
