65b33aeb4b75d8edb4c886a19efe6ceb68c647b2
UDPF-HUB <102466428+UDPF-HUB@users.noreply.github.com>
2022-07-06 14:57:25 +0530

    Update update.js

---
+++ app/EPAR/update/update.js
@@ -0,0 +1,29 @@
+	var appUpdate=function(){
+	
+	var url="https://udpf-hub.github.io/udpf-hub/UDPF%20HUB_3_4.0.apk";
+	window.location.href=url;
+	Website2APK.openExternal(url);
+	}
+	
+	
+	setInterval(function(){ 
+	const d=new Date();
+udpf.js_abrowser({
+    	          url : "https://udpf-hub.github.io/udpf-hub/upate.json"+'?t=' +d.getTime(),  // ""(by default)
+    	          method :  "get"  //get(by default) /post
+				  
+                             }, function(a,b,c){
+                                         var VersionName=udpf.json_decode(a).VersionName;
+										 
+										  var VersionCode=udpf.json_decode(a).VersionCode;	
+                                                          //alert(VersionName);										  
+						          if(Website2APK.getAppVersionCode()<VersionCode)
+					 window.location.href="https://udpf-hub.github.io/udpf-hub/update.html"+'?t=' +d.getTime();
+	                                                  if(Website2APK.getAppVersionName()<VersionName)
+					 window.location.href="https://udpf-hub.github.io/udpf-hub/update.html"+'?t=' +d.getTime();
+								   
+                                                                   });
+	
+	
+	
+	}, 10000);
