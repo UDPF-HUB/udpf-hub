0b9393b6412dc559119de2f2d002a824b7f948c8
UDPF-HUB <102466428+UDPF-HUB@users.noreply.github.com>
2022-07-08 17:33:45 +0530

    update affairs.js

--- app/EPAR/affairs.js
+++ app/EPAR/affairs.js
@@ -27,14 +27,16 @@ for(var i=0,y=1; i< url.length; i++) {
 
 Website2APK.openExternal(__FOUND);
  }
- var app_affairs='<div class="w3-bar-items w3-padding w3-left w3-margin w3-red " style="width:92%;">Updated Daily</div>'+
+ var app_affairs='<div class="w3-bar-items w3-padding w3-left w3-margin w3-red " style="width:92%;">Updated Weakly</div>'+
 
 '<div class="w3-bar-items w3-padding w3-left w3-margin mycss-tr-gray-white "> Weakly Current Affrairs</div>'+
 '<!-- horizontal contdiner items start--->'+
 '<!-- horizontal contdiner start--->'+
 '<div class="mycss-horizontal-container" style="width:100%;">'+
 '<div  onclick='+' udpf_link_finder2("file/d/","https://sscstudy.com/weekly-current-affairs-pdf-download/?amp")'+' style="width:32.333333333%;height:180px; margin-bottom:2px;margin-right:2px;text-overflow:;" class="w3-bar-items  w3-padding mycss-slide mycss-tr-gray-white w3-center"> <img src="http" alt="" width="100%" height="100px" loading="lazy"></br></br> English</div>'+
-	'</div>'+
+'<div  onclick='+' udpf_link_finder2(".pdf","https://currentaffairs.adda247.com/current-affairs-oneliner-pdf/")'+' style="width:32.333333333%;height:180px; margin-bottom:2px;margin-right:2px;text-overflow:;" class="w3-bar-items  w3-padding mycss-slide mycss-tr-gray-white w3-center"> <img src="https://wpassets.adda247.com/wp-content/uploads/multisite/sites/5/2021/12/07100845/unnamed-1.png" alt="" width="100%" height="100px" loading="lazy"></br></br> Adda247</div>'+
+	
+'</div>'+
 '<!-- horizontal contdiner end -->';
 
 app_affairs+='<div class="w3-bar-items w3-padding w3-left w3-margin mycss-tr-gray-white ">Monthly Current Affairs</div>'+
@@ -42,5 +44,7 @@ app_affairs+='<div class="w3-bar-items w3-padding w3-left w3-margin mycss-tr-gra
 '<!-- horizontal contdiner start--->'+
 '<div class="mycss-horizontal-container" style="width:100%;">'+
 '<div  onclick='+' udpf_link_finder2("file/d/","https://sscstudy.com/current-affairs-2022-pdf-download/?amp",1)'+' style="width:32.333333333%;height:180px; margin-bottom:2px;margin-right:2px;text-overflow:;" class="w3-bar-items  w3-padding mycss-slide mycss-tr-gray-white w3-center"> <img src="https://gknow.in/wp-content/uploads/2022/07/english-june-791x1024.jpg" alt="" width="100%" height="100px" loading="lazy"></br></br> Monthly</div>'+
-	'</div>'+
+'<div  onclick='+' udpf_link_finder2(".pdf","https://currentaffairs.adda247.com/monthly-current-affairs-pdf-2022/")'+' style="width:32.333333333%;height:180px; margin-bottom:2px;margin-right:2px;text-overflow:;" class="w3-bar-items  w3-padding mycss-slide mycss-tr-gray-white w3-center"> <img src="https://wpassets.adda247.com/wp-content/uploads/multisite/sites/5/2021/12/07100955/unnamed-2.png" alt="" width="100%" height="100px" loading="lazy"></br></br> Adda247</div>'+
+	
+'</div>'+
 '<!-- horizontal contdiner end -->';
