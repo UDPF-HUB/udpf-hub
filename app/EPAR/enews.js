71805368474d054574191532a2a3d4b933dfd364
UDPF-HUB <102466428+UDPF-HUB@users.noreply.github.com>
2022-07-11 17:43:55 +0530

    update enews.js

--- app/EPAR/enews.js
+++ app/EPAR/enews.js
@@ -37,7 +37,9 @@ var app_enews='<div class="w3-bar-items w3-padding w3-left w3-margin w3-red " st
 '<!-- horizontal contdiner start--->'+
 '<div class="mycss-horizontal-container" style="width:100%;">'+
 '<div  onclick='+' udpf_link_finder2("file/d/","https://www.rojgarsmachar.in/rojgar-samachar-2022/")'+' style="width:32.333333333%;height:180px; margin-bottom:2px;margin-right:2px;text-overflow:;" class="w3-bar-items  w3-padding mycss-slide mycss-tr-gray-white w3-center"> <img src="https://1.bp.blogspot.com/-OjF94ICl9bI/XvW71_LstLI/AAAAAAAABEw/QzfJZ_q3DXwJ1sFHj095l4opLRkvLxLtQCK4BGAsYHg/w320-h193/Employment%2Bnews%2Blogo.png" alt="" width="100%" height="100px" loading="lazy"></br></br> English</div>'+
-	'</div>'+
+	'<div  onclick='+' udpf_link_finder2("1.pdf","https://indianewjobs.com/employment-news-pdf-free-download/",1)'+' style="width:32.333333333%;height:180px; margin-bottom:2px;margin-right:2px;text-overflow:;" class="w3-bar-items  w3-padding mycss-slide mycss-tr-gray-white w3-center"> <img src="https://1.bp.blogspot.com/-OjF94ICl9bI/XvW71_LstLI/AAAAAAAABEw/QzfJZ_q3DXwJ1sFHj095l4opLRkvLxLtQCK4BGAsYHg/w320-h193/Employment%2Bnews%2Blogo.png" alt="" width="100%" height="100px" loading="lazy"></br></br> English -2</div>'+
+	
+'</div>'+
 '<!-- horizontal contdiner end -->';
 
 app_enews+='<div class="w3-bar-items w3-padding w3-left w3-margin mycss-tr-gray-white ">रोजगार अख़बार</div>'+
@@ -45,6 +47,8 @@ app_enews+='<div class="w3-bar-items w3-padding w3-left w3-margin mycss-tr-gray-
 '<!-- horizontal contdiner start--->'+
 '<div class="mycss-horizontal-container" style="width:100%;">'+
 '<div  onclick='+' udpf_link_finder2("file/d/","https://www.rojgarsmachar.in/rojgar-samachar-2022/",2)'+' style="width:32.333333333%;height:180px; margin-bottom:2px;margin-right:2px;text-overflow:;" class="w3-bar-items  w3-padding mycss-slide mycss-tr-gray-white w3-center"> <img src="https://i0.wp.com/naukaricareer.com/wp-content/uploads/2021/04/employment-news-weekly.png" alt="" width="100%" height="100px" loading="lazy"></br></br> हिन्दी</div>'+
-	'</div>'+
+'<div  onclick='+' udpf_link_finder2("1.pdf","https://indianewjobs.com/employment-news-pdf-free-download/",2)'+' style="width:32.333333333%;height:180px; margin-bottom:2px;margin-right:2px;text-overflow:;" class="w3-bar-items  w3-padding mycss-slide mycss-tr-gray-white w3-center"> <img src="https://i0.wp.com/naukaricareer.com/wp-content/uploads/2021/04/employment-news-weekly.png" alt="" width="100%" height="100px" loading="lazy"></br></br> हिन्दी -2</div>'+
+	
+'</div>'+
 '<!-- horizontal contdiner end -->';
 
