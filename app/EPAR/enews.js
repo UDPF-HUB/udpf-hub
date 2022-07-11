a2bc106f1ba977f00acdecb809d191bbe26eaf88
UDPF-HUB <102466428+UDPF-HUB@users.noreply.github.com>
2022-07-11 15:53:33 +0530

    Update enews.js

--- app/EPAR/enews.js
+++ app/EPAR/enews.js
@@ -36,7 +36,7 @@ var app_enews='<div class="w3-bar-items w3-padding w3-left w3-margin w3-red " st
 '<!-- horizontal contdiner items start--->'+
 '<!-- horizontal contdiner start--->'+
 '<div class="mycss-horizontal-container" style="width:100%;">'+
-'<div  onclick='+' udpf_link_finder2("file/d/","https://www.rojgarsmachar.in/rojgar-samachar-2022/")'+' style="width:32.333333333%;height:180px; margin-bottom:2px;margin-right:2px;text-overflow:;" class="w3-bar-items  w3-padding mycss-slide mycss-tr-gray-white w3-center"> <img src="https://www.rojgarsmachar.in/wp-content/uploads/Rojgar-Samachar-2022.jpg" alt="" width="100%" height="100px" loading="lazy"></br></br> English</div>'+
+'<div  onclick='+' udpf_link_finder2("file/d/","https://www.rojgarsmachar.in/rojgar-samachar-2022/")'+' style="width:32.333333333%;height:180px; margin-bottom:2px;margin-right:2px;text-overflow:;" class="w3-bar-items  w3-padding mycss-slide mycss-tr-gray-white w3-center"> <img src="https://1.bp.blogspot.com/-OjF94ICl9bI/XvW71_LstLI/AAAAAAAABEw/QzfJZ_q3DXwJ1sFHj095l4opLRkvLxLtQCK4BGAsYHg/w320-h193/Employment%2Bnews%2Blogo.png" alt="" width="100%" height="100px" loading="lazy"></br></br> English</div>'+
 	'</div>'+
 '<!-- horizontal contdiner end -->';
 
@@ -44,7 +44,7 @@ app_enews+='<div class="w3-bar-items w3-padding w3-left w3-margin mycss-tr-gray-
 '<!-- horizontal contdiner items start--->'+
 '<!-- horizontal contdiner start--->'+
 '<div class="mycss-horizontal-container" style="width:100%;">'+
-'<div  onclick='+' udpf_link_finder2("file/d/","https://www.rojgarsmachar.in/rojgar-samachar-2022/",2)'+' style="width:32.333333333%;height:180px; margin-bottom:2px;margin-right:2px;text-overflow:;" class="w3-bar-items  w3-padding mycss-slide mycss-tr-gray-white w3-center"> <img src="https://www.rojgarsmachar.in/wp-content/uploads/Rojgar-Samachar-2022.jpg" alt="" width="100%" height="100px" loading="lazy"></br></br> हिन्दी</div>'+
+'<div  onclick='+' udpf_link_finder2("file/d/","https://www.rojgarsmachar.in/rojgar-samachar-2022/",2)'+' style="width:32.333333333%;height:180px; margin-bottom:2px;margin-right:2px;text-overflow:;" class="w3-bar-items  w3-padding mycss-slide mycss-tr-gray-white w3-center"> <img src="https://i0.wp.com/naukaricareer.com/wp-content/uploads/2021/04/employment-news-weekly.png" alt="" width="100%" height="100px" loading="lazy"></br></br> हिन्दी</div>'+
 	'</div>'+
 '<!-- horizontal contdiner end -->';
 
