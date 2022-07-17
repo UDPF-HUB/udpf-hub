b1efd77915e65de10d17621f091a56c0c768c1de
UDPF-HUB <102466428+UDPF-HUB@users.noreply.github.com>
2022-07-17 20:00:47 +0530

    update affairs.js

--- app/EPAR/affairs.js
+++ app/EPAR/affairs.js
@@ -1,32 +1,4 @@
-// Image link On error
-$('#app-main').on('DOMSubtreeModified', function(entries) {
-	//    for(var i=0; i<$("img").length;i++){
-	      //  $("img")[i].setAttribute("loading","lazy");
-	// }
-	$("img").error(function(){ console.log("Image Broken");this.src='https://reactnativecode.com/wp-content/uploads/2018/02/Default_Image_Thumbnail.png';});		
-                        
-});
 
-function udpf_link_finder2(filter, url="url",ind=1){
-var op=udpf.js_browser(proxy_url(url)).trim();	
-var urlRegex = /(?:(?:https?|ftp|file):\/\/|www\.|ftp\.)(?:\([-A-Z0-9+&@#\/%=~_|$?!:,.]*\)|[-A-Z0-9+&@#\/%=~_|$?!:,.])*(?:\([-A-Z0-9+&@#\/%=~_|$?!:,.]*\)|[A-Z0-9+&@#\/%=~_|$])/igm;
-var url = op.match(urlRegex);
-
-//Link Search
-var __FOUND ="";
-for(var i=0,y=1; i< url.length; i++) {
-	var ttext=(url[i]).toString() ;
-	//console.log(i+ttext)
-	if( ttext.toUpperCase().indexOf(filter.toUpperCase()) > -1) {
-		__FOUND = url[i]; 
-		if(ind==y)
-		break;
-		y++;
-	}
-}
-
-Website2APK.openExternal(__FOUND);
- }
  var app_affairs='<div class="w3-bar-items w3-padding w3-left w3-margin w3-red " style="width:92%;">Updated Weakly</div>'+
 
 '<div class="w3-bar-items w3-padding w3-left w3-margin mycss-tr-gray-white "> Weakly Current Affrairs</div>'+
